1b7a7055be4e3b5086525bea9daddb667a6f3eab
yanomane <ricardo.yanomane.166@gmail.com>
2021-01-13 14:46:00 -0300

    My Django Girls app, first commit

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome','email','telefone')
+admin.site.register(Contato, ContatoAdmin)
+
